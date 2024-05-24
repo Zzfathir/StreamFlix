027c12d2e5114455b1d0e3bdc9620116c1d3f322
Zzfathir <ahmadfathir0830@gmail.com>
2024-05-24 09:44:15 +0700

    7 - Menentukan harga film berdasarkan average vote & menampilkannya.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 import { Container, Grid, Card, CardMedia, CardContent, Typography, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { fetchNowPlayingMovies } from "../services/api";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
@@ -20,6 +20,18 @@ function MovieList() {
     setPage((prevPage) => prevPage + 1);
   }
 
+  function moviePrice(rating) {
+    if (rating <= 3) {
+      return 3500;
+    } else if (rating <= 6) {
+      return 8250;
+    } else if (rating <= 8) {
+      return 16350;
+    } else {
+      return 21250;
+    }
+  }
+
   return (
     <Container>
       <Typography variant="h4" gutterBottom>
@@ -32,9 +44,9 @@ function MovieList() {
               <CardMedia component="img" image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
               <CardContent>
                 <Typography variant="h6">{movie.title}</Typography>
-                <Typography variant="body2">Price:</Typography>
-                <Button component={Link} >Detail</Button>
-              </CardContent>    
+                <Typography variant="body2">Price: Rp. {moviePrice(movie.vote_average)}</Typography>
+                <Button component={Link}>Detail</Button>
+              </CardContent>
             </Card>
           </Grid>
         ))}
