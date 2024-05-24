e926b9f2283d4631fe56f141ad9d91477e0c6cbb
Zzfathir <ahmadfathir0830@gmail.com>
2024-05-24 11:17:09 +0700

    11 - Menampilkan Konten Detail Movie

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,19 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { fetchMovieDetail } from '../services/api';
-import { Container, Typography, Card, CardMedia, CardContent, Button } from '@mui/material';
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMovieDetail } from "../services/api";
+import { Container, Typography, Card, CardMedia, CardContent, Button } from "@mui/material";
 
 function MovieDetail() {
-    const {movieId} = useParams()
-    const [movie, setMovies] = useState(null)
+  const { movieId } = useParams();
+  const [movie, setMovies] = useState(null);
 
-    useEffect(() => {
-        async function loadMovie() {
-            const movieData = await fetchMovieDetail(movieId)
-            setMovies(movieData)
-        }
+  useEffect(() => {
+    async function loadMovie() {
+      const movieData = await fetchMovieDetail(movieId);
+      setMovies(movieData);
+    }
 
-        loadMovie()
-    }, [movieId])
+    loadMovie();
+  }, [movieId]);
+
+  return (
+    <Container>
+      <Card>
+        <CardMedia component="img" image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+        <CardContent>
+          <Typography variant="h4">{movie.title}</Typography>
+          <Typography variant="body1">Rating: {movie.vote_average}</Typography>
+          <Typography variant="body1">Durasi: {movie.runtime} menit</Typography>
+          <Typography variant="body1">Harga: Rp. {getMoviePrice(movie.vote_average)}</Typography>
+          </CardContent>
+      </Card>
+    </Container>
+  );
 }
